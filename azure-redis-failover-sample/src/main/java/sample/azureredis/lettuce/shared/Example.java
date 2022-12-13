fd2ab610ef64c05a374ada71ffef6ed5993e7087
Chris Rice <ricec121@gmail.com>
2022-12-13 16:10:24 -0600

    Limit executor queue and print executor usage

--- azure-redis-failover-sample/src/main/java/sample/azureredis/lettuce/shared/Example.java
+++ azure-redis-failover-sample/src/main/java/sample/azureredis/lettuce/shared/Example.java
@@ -6,30 +6,37 @@ public abstract class Example implements AutoCloseable {
     public abstract void executeWrite();
 
     public void run() throws InterruptedException {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor)Executors.newFixedThreadPool(8);
+        BlockingQueue q = new ArrayBlockingQueue(1000);
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8, 20, TimeUnit.SECONDS, q);
+        
         int count = 0;
+        int rejectedCount = 0;
         while(true)
         {
-            if (count % 40 == 0) {
-                System.out.println("Writing to Redis... (" + executor.getCompletedTaskCount() + " operations completed)");
+            if (count % 500 == 0) {
+                System.out.printf("Writing to Redis... (Active: %d, Queued: %d, Completed: %d, Rejected: %d)\n",
+                    executor.getActiveCount(),
+                    executor.getQueue().size(),
+                    executor.getCompletedTaskCount(),
+                    rejectedCount);
             }
 
-            for (int i = 0; i < 8; i++) {
+            try {
                 executor.submit(() -> {
-                    try
-                    {
+                    try {
                         executeWrite();
                     }
-                    catch (Exception e)
-                    {
+                    catch (Exception e) {
                         System.out.println("Failed to write to Redis.");
                         System.out.println(e.getMessage());
                     }
                 });
+            } catch (RejectedExecutionException e) {
+                rejectedCount++;
             }
-
+            
+            Thread.sleep(8);
             count++;
-            Thread.sleep(50);
         }
     }
 }
